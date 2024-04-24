bfd7b39620121f3340a568db11559e63b695eca2
VickyIbarra <vickrojas9@gmail.com>
2024-04-24 17:35:25 -0300

    agregando main.js

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/*document.querySelector("header").innerHTML=`
+            <a href="#home">Home</a>
+            <a href="#nosotros">Sobre nosotros</a>
+            <a href="./productos.html">Productos</a>
+            <a href="#contacto">Contacto</a>
+            <a href="#sucursales">Sucursales</a>
+`
+
+document.querySelector("footer").innerHTML=`
+<div class="grupo-1">
+<div class="box">
+    <figure>
+        <a href="#">
+            <img src="./img/Logo_Nono2.png" alt="logo empresa">
+        </a>
+    </figure>
+</div>
+<div class="box">
+    <h2> agregar algun texto de sucursales,telefonos o informacion util</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi reiciendis minima provident recusandae
+        ea, repellendus modi quod cum repellat possimus, soluta consectetur tempora voluptates id facilis
+        veritatis nihil, laboriosam pariatur.</p>
+</div>
+<div class="box">
+    <h2>Seguinos en nuestras redes</h2>
+    <div class="red-social">
+        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+    </div>
+</div>
+</div>
+
+<div class="grupo-2">
+<small> @ copyright 2024.<b>4IdeasDev</b> - Todos los derechos reservados.CaC - Fullstack Python 2004 -
+    Proyecto Grupal 1 - Grupo 16</p></small>
+</div>
+`*/
