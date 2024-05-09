2775a0eedaf1cf8b7e18e7dd3feac89d3ba882b5
BaezPamela <baezpamelaj@gmail.com>
2024-05-08 21:03:38 -0300

    agrego select al formulario y consumo de Api. agrego json de productos

---
+++ js/formulario.js
@@ -0,0 +1,19 @@
+const d = document;
+const selectProv= d.getElementById("selectProvincias");
+
+function provincia(){
+    fetch( "https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre")
+    .then(res => res.ok ? res.json(): Promise.reject(res))
+    .then(json => {
+        let opciones = `<option value="Elige una provincia"> Elige una provincia</option> `
+
+        json.provincias.forEach(el => opciones += `<option value="${el.nombre}">${el.nombre}</option> `);
+            
+        selectProv.innerHTML = opciones;
+        });
+            
+}
+    
+    
+
+d.addEventListener("DOMContentLoaded",provincia)
