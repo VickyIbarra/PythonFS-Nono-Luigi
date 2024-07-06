29ed2d4e9c4517b0c6c12436576b28471eaf6ce5
JPFinal <jpfinal@gmail.com>
2024-07-06 17:55:53 -0300

    modicaciones al cc2
    Modificaciones al cc2 y a los htmls de productos_info

--- Front_End/js/codigo_barras.js
+++ Front_End/js/codigo_barras.js
@@ -1,7 +1,8 @@
 JsBarcode("#barcode", "12313", {
-    format: "codabar",
-    lineColor: "#0aa",
-    width: 4,
-    height: 40,
-    displayValue: true,
-  });
+  format: "codabar",
+  lineColor: "rgb(148, 125, 108)", // Cambia el color de las líneas a negro
+  background: "#fff", // Cambia el color de fondo a blanco
+  width: 4,
+  height: 40,
+  displayValue: true,
+});
